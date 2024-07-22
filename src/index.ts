1f03622b2be0d2b200aa9fa08a67f6587984870a
heiwa4126 <heiwa4126@gmail.com>
2024-07-22 13:35:23 +0900

    add some example codes on https://hono.dev/docs/getting-started/basic

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,23 @@ app.get("/", (c) => {
 	return c.text("Hello Hono!");
 });
 
+app.get("/api/hello", (c) => {
+	return c.json({
+		ok: true,
+		message: "Hello Hono!",
+	});
+});
+
+app.get("/posts/:id", (c) => {
+	const page = c.req.query("page");
+	const id = c.req.param("id");
+	c.header("X-Message", "Hi!");
+	return c.text(`You want see ${page} of ${id}`);
+});
+
+app.post("/posts", (c) => c.text("Created!", 201));
+app.delete("/posts/:id", (c) => c.text(`${c.req.param("id")} is deleted!`));
+
 const port = 3000;
 console.log(`Server is running on port ${port}`);
 
