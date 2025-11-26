7fccfb88c0856374e1188c2f13f64607475b7e56
heiwa4126 <heiwa4126@gmail.com>
2025-11-26 16:38:23 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { serve } from "@hono/node-server";
 import app from "./app.js";
 
 const port = 3000;
-console.log(`Server is running on port ${port}`);
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
 	fetch: app.fetch,
