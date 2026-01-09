fbd1977d0169ef04cc068df4ec246f8e90848863
heiwa4126 <heiwa4126@gmail.com>
2026-01-09 13:39:33 +0900

    remove jest and add vitest

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,13 +1,15 @@
-import { expect, it } from "@jest/globals";
 import { testClient } from "hono/testing";
+import { describe, expect, it } from "vitest";
 import app from "./app.js";
 
-it("test", async () => {
-	const client = testClient(app);
-	const res = await client.api.hello.$get();
-	expect(res.status).toBe(200);
-	expect(await res.json()).toEqual({
-		ok: true,
-		message: "Hello Hono!",
+describe("app", () => {
+	it("should return hello response", async () => {
+		const client = testClient(app) as any;
+		const res = await client.api.hello.$get();
+		expect(res.status).toBe(200);
+		expect(await res.json()).toEqual({
+			ok: true,
+			message: "Hello Hono!",
+		});
 	});
 });
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		globals: true,
+		environment: "node",
+		exclude: [
+			"tests-e2e/**/*.spec.ts",
+			// "node_modules/**/*.{test,spec}.{js,ts}",
+			"**/node_modules/**",
+		],
+	},
+});
